deea0d6b89bff9563e5322e5dd7e25be3fef5d7d
amittimes <amit.info8@gmail.com>
2024-01-15 21:31:39 +0530

    My 1st commit for git leraning

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,13 @@
+package com.qa.pages;
+
+public class HomePage {
+
+
+		// TODO Auto-generated method stub
+
+		public void homePage()
+		{
+			System.out.println("Home Page");
+		}
+	
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,16 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	
+	@Test
+	public void loginTest()
+	{
+		
+		System.out.println("Amit Pandey");
+		
+	}
+}
+
